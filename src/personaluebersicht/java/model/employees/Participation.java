2a4f1af88f121f5c2e839d7342314431d9265f60
diego <fantinod@bzz.ch>
2022-06-22 11:14:05 +0200

    added Javadoc into personaluebersicht.java.model.employees

--- src/personaluebersicht/java/model/employees/Participation.java
+++ src/personaluebersicht/java/model/employees/Participation.java
@@ -2,57 +2,106 @@ package personaluebersicht.java.model.employees;
 
 import java.util.ArrayList;
 
+/**
+ * a Participation
+ * @author Diego Fantino
+ * @since 22.06.2022
+ * @version 1.0
+ */
 public class Participation {
     private ArrayList<String> function;
     private ArrayList<String> teams;
     private Person owner;
 
+    /**
+     * public constructor
+     * @param person the owner of the participation
+     */
     public Participation(Person person) {
         this.owner = person;
         function = new ArrayList<>();
         teams = new ArrayList<>();
     }
 
+    /**
+     * adds a function into function arraylist
+     * @param function the function to add
+     */
     public void addFunction(String function) {
         this.function.add(function);
     }
 
+    /**
+     * gets the Functionname from the function arraylist
+     * @param index of the function in function arraylist
+     * @return the function name as String
+     */
     public String getFunctionName(int index) {
-        // Potentielle Fehlerquelle
         return function.get(index);
     }
 
+    /**
+     * removes a function from the function arraylist
+     * @param index of the function in function arraylist
+     */
     public void removeFunction(int index) {
         function.remove(index);
     }
 
+    /**
+     * gets amount of functions in function arraylist
+     * @return int amount of functions in function arraylist
+     */
     public int getNumberOfFunctions() {
         return function.size();
     }
 
+    /**
+     * adds a team in teams arraylist
+     * @param teams team to add
+     */
     public void addTeam(String teams) {
         this.teams.add(teams);
     }
 
+    /**
+     * gets the team name from team in teams arraylist
+     * @param index of the team in teams arraylist
+     * @return the teams name
+     */
     public String getTeamName(int index) {
-        // potentielle Fehlerquelle
         return teams.get(index);
     }
 
+    /**
+     * removes a team from the teams arraylist
+     * @param index pof the team in teams arraylist
+     */
     public void removeTeam(int index) {
         teams.remove(index);
     }
 
+    /**
+     * gets amount of teams in teams arraylist
+     * @return int the amount of teams
+     */
     public int getNumberOfTeams() {
         return teams.size();
     }
 
+    /**
+     * gets the owner of this participation
+     * @return owner the owner
+     */
     public Person getOwner() {
         return owner;
     }
 
+    /**
+     * sets the owner of this participation
+     * @param owner the owner of this participation
+     */
     public void setOwner(Person owner) {
         this.owner = owner;
     }
-
 }
--- src/personaluebersicht/java/model/employees/Person.java
+++ src/personaluebersicht/java/model/employees/Person.java
@@ -1,37 +1,73 @@
 package personaluebersicht.java.model.employees;
 
+/**
+ * a Person
+ * @author Diego Fantino
+ * @since 22.06.2022
+ * @version 1.0
+ */
 public class Person {
     private Picture photo;
     private String firstName;
     private String lastName;
     private Participation participation;
 
+    /**
+     * public constructor
+     * @param fName the Firstname of the Person
+     * @param lName the Lastname of the Person
+     * @param photo a photo of the Person
+     */
     public Person(String fName, String lName, Picture photo) {
         this.photo = photo;
         this.firstName = fName;
         this.lastName = lName;
     }
 
+    /**
+     * sets the firstname of the person
+     * @param firstName of the person
+     */
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
 
+    /**
+     * sets the lastname of the person
+     * @param lastName of the person
+     */
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
 
+    /**
+     * sets the photo of the person
+     * @param photo the photo
+     */
     public void setPhoto(Picture photo) {
         this.photo = photo;
     }
 
+    /**
+     * gets the firstname of the person
+     * @return firstName of the person
+     */
     public String getFirstName() {
         return firstName;
     }
 
+    /**
+     * gets the lastname of the person
+     * @return lastname of the person
+     */
     public String getLastName() {
         return lastName;
     }
 
+    /**
+     * gets the photo of the person
+     * @return photo of the person
+     */
     public Picture getPhoto() {
         return photo;
     }
